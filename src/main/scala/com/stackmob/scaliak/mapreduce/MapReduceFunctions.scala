a3726a8a26f6d81986a8e1beb7421ab397aec418
Gideon de Kok <gideondk@me.com>
2012-05-03 17:34:56 +0200

    Default reduce identity to non keeping

--- src/main/scala/com/stackmob/scaliak/mapreduce/MapReduceFunctions.scala
+++ src/main/scala/com/stackmob/scaliak/mapreduce/MapReduceFunctions.scala
@@ -6,8 +6,8 @@ object MapReduceFunctions {
 	def mapValuesToJson(keep: Boolean = true) = MapPhase(named("Riak.mapValuesJson"), keep)
 	def mapValuesToJson:MapPhase = mapValuesToJson(true)
 	
-	def reduceIdentity(keep: Boolean = true) = ReducePhase(named("Riak.reduceIdentity"), keep)
-	def reduceIdentity:ReducePhase = reduceIdentity(true)
+	def reduceIdentity(keep: Boolean = false) = ReducePhase(named("Riak.reduceIdentity"), keep)
+	def reduceIdentity:ReducePhase = reduceIdentity(false)
 	
 	def sort(field: String, sortDESC: Boolean = false, keep: Boolean = true) = ReducePhase(anon(""" 
 	function(values) {
